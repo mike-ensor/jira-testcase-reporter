53885c97a24237eb37545ab694a49897de131c91
Mike! <mike@clickconcepts.com>
2013-02-25 07:13:24 -0800

    * Fixing syntax

--- src/main/java/com/clickconcepts/jira/TestCase.java
+++ src/main/java/com/clickconcepts/jira/TestCase.java
@@ -1,20 +1,21 @@
-package com.clickconcepts.jira
+package com.clickconcepts.jira;
 
-import java.lang.annotation.ElementType
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-import java.lang.annotation.Target
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * This is a marker annotation that will signify which JIRA Test Case the automated test is for.  When hooked up, the results will be transferred to JIRA.  If all of the same number pass, success, if one ore more fail, then failure
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target([ElementType.TYPE, ElementType.METHOD])
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface TestCase {
 
     /**
      * Links back to a test case in JIRA
+     *
      * @return
      */
-    String value()
+    String value();
 }
